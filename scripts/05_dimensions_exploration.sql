e7816135fff28a4828c21620f7ee9fdb0783a63b
Tarun Kumar <35755650+k178412@users.noreply.github.com>
2025-05-17 02:15:56 +0530

    Update 05_dimensions_exploration.sql

--- scripts/05_dimensions_exploration.sql
+++ scripts/05_dimensions_exploration.sql
@@ -1,4 +1,8 @@
--->> DIMENSIONS EXLPORATION
+/*
+Script: dimensions_exploration.sql
+
+This script explores different dimensions within the database.
+*/
 
 --Explore all countries customers come from
 select distinct country from gold.dim_customers;

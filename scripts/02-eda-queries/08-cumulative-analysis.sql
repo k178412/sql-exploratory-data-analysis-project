4e7e03109def2168a7762c66420672028d2c4d5b
Tarun Kumar <35755650+k178412@users.noreply.github.com>
2025-06-07 15:30:13 +0530

    Create 08-cumulative-analysis.sql

---
+++ scripts/02-eda-queries/08-cumulative-analysis.sql
@@ -0,0 +1,17 @@
+/*
+Script: 08-cumulative-analysis.sql
+
+This script analyzes cumulative sales trends over time.
+It calculates total sales for each month, tracks running total sales, and computes a moving average for prices. 
+*/
+
+--Find total sales, running_total_sales, moving_average, percent difference from previous sales
+select
+	d.start_of_month,
+	sum(s.sales_amount) as total_sales,
+	sum(sum(s.sales_amount)) over(order by d.start_of_month) as running_total_sales,
+	avg(avg(s.price)) over(order by d.start_of_month) as moving_avg_price
+from gold.dim_dates d
+left join gold.fact_sales s on s.order_date = d.date_key
+group by d.start_of_month
+order by d.start_of_month;

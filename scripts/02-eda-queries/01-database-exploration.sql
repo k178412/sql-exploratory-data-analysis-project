99a7afb3ad65a4b43d1eaaed5ed5edfad785324a
Tarun Kumar <35755650+k178412@users.noreply.github.com>
2025-06-07 15:20:22 +0530

    Create 01-database-exploration.sql

---
+++ scripts/02-eda-queries/01-database-exploration.sql
@@ -0,0 +1,15 @@
+/*
+Script: 01-database-exploration.sql
+
+This script helps explore the database structure.
+*/
+
+--Explore all objetcs in the database
+select * from information_schema.tables;
+
+--Explore all columns in the database
+select* from information_schema.columns;
+
+--Explore columns for a specific table
+select* from information_schema.columns
+where table_name = 'dim_customers';

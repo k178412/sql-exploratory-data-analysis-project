3f49a2ca5ec95a1bdd5e5ecd8160b8667d7616c3
Tarun Kumar <35755650+k178412@users.noreply.github.com>
2025-06-07 18:09:39 +0530

    Update 09-performance-analysis.sql

--- scripts/02-eda-queries/09-performance-analysis.sql
+++ scripts/02-eda-queries/09-performance-analysis.sql
@@ -4,29 +4,30 @@ Performance Analysis
 This script compares current value with target value and helps compare performance.
 */
 
---Analyze the yearly performance of products by comparing each product's sales to both its average sales and previous sales.
-with total_sales_cte as(
+--Analyze the yearly performance of subcategories by comparing each subcategories' sales to both its average sales and previous sales.
+with cte as(
 select
-	p.product_name,
-	year(s.order_date) as order_year,
-	sum(s.sales_amount) as total_sales
+	p.subcategory,
+	year(s.order_date) as year,
+	sum(s.sales_amount) as total_sales,
+	avg(sum(s.sales_amount)) over(partition by p.subcategory) as avg_sales,
+	lag(sum(s.sales_amount),1,0) over(partition by p.subcategory order by year(s.order_date)) as prev_sales
 from gold.fact_sales s
-left join gold.dim_products p on p.product_key = s.product_key
-group by p.product_name, year(s.order_date)
+left join gold.dim_products p on p.product_key=s.product_key
+where s.order_date is not null
+group by p.subcategory, year(s.order_date)
 )
-select	
-	product_name,
-	order_year,
-	total_sales,
-	avg(total_sales) over(partition by product_name) as avg_sale,
-	total_sales - avg(total_sales) over(partition by product_name) as avg_diff,
-	 case when total_sales - avg(total_sales) over(partition by product_name) > 0 then 'above avg'
-			   when total_sales - avg(total_sales) over(partition by product_name) < 0 then 'below avg'
-	end as avg_diff_remark,
-	lag(total_sales, 1, 0) over(partition by product_name order by order_year) as prev_sale,
-	total_sales - lag(total_sales, 1, 0) over(partition by product_name order by order_year) as prevsale_diff,
-	case when total_sales - lag(total_sales, 1, 0) over(partition by product_name order by order_year) > 0 then 'above prev sale'
-			  when total_sales - lag(total_sales, 1, 0) over(partition by product_name order by order_year) < 0 then 'below prev sale'
-	end as prevsale_diff_remark
-from total_sales_cte
-order by product_name, order_year;
+select
+	*,
+	total_sales - avg_sales as diff_avg,
+	case when total_sales - avg_sales > 0 then 'above avg'
+				when total_sales - avg_sales < 0 then 'below avg'
+				else 'same as avg'
+	end as diff_avg_remark,
+	total_sales - prev_sales as diff_prev,
+	case when total_sales - prev_sales > 0 then 'above prev'
+				when total_sales - avg_sales < 0 then 'below prev'
+				else 'same as prev'
+	end as diff_prev_remark
+from cte
+order by subcategory, year;

3ba46daea599bb78b0da946f32a98417f6198301
Tarun Kumar <35755650+k178412@users.noreply.github.com>
2025-06-07 15:24:22 +0530

    Create 03-daterange-exploration.sql

---
+++ scripts/02-eda-queries/03-daterange-exploration.sql
@@ -0,0 +1,20 @@
+/*
+Script: 03-daterange-exploration.sql
+
+This script explores date boundaries in the dataset.
+*/
+
+--Find the first and last order date
+select 
+	min(order_date) as first_order, 
+	max(order_date) as last_order,
+	datediff(year, min(order_date), max(order_date)) as order_range_year
+from gold.fact_sales;
+
+--Find the youngest and oldest customer
+select
+	min(birth_date) as oldest_cust_birthdate,
+	datediff(year, min(birth_date), getdate()) as oldest_cust_age,
+	max(birth_date) as youngest_cust_birthdate,
+	datediff(year, max(birth_date), getdate()) as youngest_cust_age
+from gold.dim_customers;
